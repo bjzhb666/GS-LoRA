97edcb1bb54f29fbfe7ac5b097c5d56323ae0e29
bjzhb666 <bjzhb666@outlook.com>
2023-10-16 17:24:09 +0800

    add change label wrapper

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -16,3 +16,22 @@ def build_dataset(image_set, args, cls_order, phase_idx, incremental, incrementa
     if args.dataset_file == 'coco':
         return build_coco(image_set, args, cls_order, phase_idx, incremental, incremental_val, val_each_phase, balanced_ft, is_rehearsal)
     raise ValueError(f'dataset {args.dataset_file} not supported')
+
+import torch
+class CLDatasetWrapper:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, index):
+        image, data = self.dataset[index]
+        new_data = data.copy()
+
+        if 'labels' in new_data:
+            labels = new_data['labels']
+            if labels.numel() > 0:
+                new_data['labels'] = torch.full_like(labels, 12)
+
+        return image, new_data
+
+    def __len__(self):
+        return len(self.dataset)
