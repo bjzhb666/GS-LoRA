507eae76c0eb2d3860fd9a3bba20f2dec6e7af41
bjzhb666 <bjzhb666@outlook.com>
2024-03-18 20:21:10 +0800

    remove unrelated files

--- Face-Transformer/Tsne/train.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-python3 -u train_own.py -b 96 -w 0 -d tsne -n VIT -e 500 \
--head CosFace --outdir ./results/Tsne-depth6-500ep \
---warmup-epochs 5 --lr 3e-4 --num_workers 8  --lora_rank 0 --decay-epochs 400 \
---vit_depth 6
