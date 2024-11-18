e0c0100a79dc35f0d1ddb9609dbd08119ec8c939
bjzhb666 <bjzhb666@outlook.com>
2024-11-18 19:56:47 +0800

    fix bugs of FDR DER dataloader cl forget

--- scripts/run_cl_baseline.sh
+++ scripts/run_cl_baseline.sh
@@ -3,7 +3,7 @@
 NUM_FIRST_CLS=70
 CLS_PER_PHASE=$((80 - $NUM_FIRST_CLS))
 
-lr=3e-4
+lr=6e-4
 # # retrain
 # GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
 #         --batch_size 2 --output_dir ./exps-CL/gpu \
@@ -13,33 +13,33 @@ lr=3e-4
 #         --seed_cls 123  --l2_lambda 0 \
 #         --num_tasks 7 --retrain
 
-# # l2
-# GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
-#         --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-l2 \
-#         --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
-#         --epoch 30 --lr $lr --cache_mode  \
-#         --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
-#         --seed_cls 123  --l2_lambda 0.01\
-#         --num_tasks 2 --l2 --replay 
+# l2
+GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
+        --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-l2 \
+        --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
+        --epoch 30 --lr $lr --cache_mode  \
+        --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
+        --seed_cls 123  --l2_lambda 0.01\
+        --num_tasks 7 --l2 --replay 
 
 
-# # ewc
-# GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 39500 ./configs/r50_deformable_detr_CL_baseline.sh \
-#         --batch_size 2 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-ewc \
-#         --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
-#         --epoch 30 --lr $lr --cache_mode  \
-#         --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
-#         --seed_cls 123  --ewc_lambda 10 \
-#         --num_tasks 1 --ewc --replay
+# ewc
+GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 39500 ./configs/r50_deformable_detr_CL_baseline.sh \
+        --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-ewc \
+        --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
+        --epoch 30 --lr $lr --cache_mode  \
+        --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
+        --seed_cls 123  --ewc_lambda 10 \
+        --num_tasks 7 --ewc --replay
 
-# # mas
-# GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
-#         --batch_size 2 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-mas \
-#         --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
-#         --epoch 30 --lr $lr --cache_mode  \
-#         --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
-#         --seed_cls 123  --mas_lambda 0.1 \
-#         --num_tasks 1 --MAS  --replay
+# mas
+GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
+        --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-mas \
+        --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
+        --epoch 30 --lr $lr --cache_mode  \
+        --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE  --no_aux_loss \
+        --seed_cls 123  --mas_lambda 0.1 \
+        --num_tasks 7 --MAS  --replay
 
 # # DER
 # GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 --master_port 29500 ./configs/r50_deformable_detr_CL_baseline.sh \
@@ -68,13 +68,13 @@ lr=3e-4
 #         --seed_cls 123  \
 #         --num_tasks 7 --FDR --FDR_lambda 0.1 --wandb_offline
 
-# SCRUB
-GPUS_PER_NODE=8 ./tools/run_dist_launch.sh  8  --master_port 29500 \
-        ./configs/r50_deformable_detr_CL_baseline.sh \
-        --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-SCRUB \
-        --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
-        --epoch 30 --lr $lr --cache_mode \
-        --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE --no_aux_loss \
-        --seed_cls 123 \
-        --num_tasks 7 --SCRUB_superepoch 3 --sgda_smoothing 0 --SCRUB  # --wandb_offline \
-         # --few_shot --few_shot_num 20
+# # SCRUB
+# GPUS_PER_NODE=8 ./tools/run_dist_launch.sh  8  --master_port 29500 \
+#         ./configs/r50_deformable_detr_CL_baseline.sh \
+#         --batch_size 4 --output_dir ./exps-CL/start${NUM_FIRST_CLS}-each${CLS_PER_PHASE}-SCRUB \
+#         --resume /data3/hongbo_zhao/CL-DETR/exps/r50_deformable_detr/r50_deformable_detr-checkpoint.pth \
+#         --epoch 30 --lr $lr --cache_mode \
+#         --num_of_first_cls $NUM_FIRST_CLS --cls_per_phase $CLS_PER_PHASE --no_aux_loss \
+#         --seed_cls 123 \
+#         --num_tasks 7 --SCRUB_superepoch 3 --sgda_smoothing 0 --SCRUB  # --wandb_offline \
+#          # --few_shot --few_shot_num 20
